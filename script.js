a77de147c2e94b60af1d9476af2c25f5f0f5edcc
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-02-07 20:56:05 +0200

    Add Remove Book function

--- script.js
+++ script.js
@@ -23,10 +23,13 @@ function addBookToLibrary(book,author,numPages) {
 
 // Display book on site
 function Book() {
+    let x = 0
     let tuksa = [myLibrary[myLibrary.length - 1]]
+
     tuksa.forEach(function(book) {
         const books = document.querySelector(".books");
         const p = document.createElement("p");
+        p.setAttribute('class',`id${x}`)
         p.textContent = `Title: ${book.book} Author: ${book.author} Pages: ${book.numberOfPages}`;
         const button = document.createElement('button')
         button.setAttribute('class','allDeleteButtons')
@@ -37,7 +40,21 @@ function Book() {
 
         // Remove Book Button 
         button.addEventListener("click", () => {
-            
+            const delBooks = document.querySelectorAll('.allDeleteButtons');
+            delBooks.forEach(function(bookId) {
+                if (parseInt(bookId.id.slice(3)) === myLibrary[x]['id']){
+                    myLibrary.splice(x,1);
+                    const delElement = document.getElementById(`${bookId.id}`)
+                    delElement.remove()
+                    const delButton = document.querySelector(`.id${x}`)
+                    console.log(bookId.id)
+                    delButton.remove()
+                }
+
+                x++
+                
+            });
+            x = 0;
         });
     });
 }

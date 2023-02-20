f41874eb88f55f62f0125f8206121e661c18e563
LukaTm <119138345+LukaTm@users.noreply.github.com>
2023-02-20 12:23:59 +0200

    Fix bugs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 
 let myLibrary = [];
+let x = 0
 
 // Add Book's all info to myLibrary
 const form = document.querySelector("form");
@@ -7,77 +8,104 @@ form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     // Book Read? Check
-    let readStatusYes = undefined
-    document.querySelector("#yes").checked ? readStatusYes = "Read" : readStatusYes = "Not Read" 
+    let readStatus = undefined
+    document.querySelector("#yes").checked ? readStatus = "Read" : readStatus = "Not Read" 
 
     const book = document.querySelector("#book").value;
     const author = document.querySelector("#author").value;
     const pages = document.querySelector("#pages").value;
-    addBookToLibrary(book,author,pages,readStatusYes);
+    addBookToLibrary(book,author,pages,readStatus);
 });
 
-
-
+// Add Book to myLibrary
 function addBookToLibrary(book,author,numPages,readStatus) {
     myLibrary.push ({
         'book': book,
         'author': author,
         'numberOfPages':numPages,
-        'id':myLibrary.length,
-        'readStatus': readStatus
+        'id':x,
+        'readStatus': readStatus,
+        'readBool': readStatus == 'Not Read' ? false : true
     })
     Book()
+}
+
+function changeReadStatus() {
+
 }
 
 
 // Display book on site
 const books = document.querySelector(".books");
 function Book() {
-    let x = 0
     let tuksa = [myLibrary[myLibrary.length - 1]]
-
-    tuksa.forEach(function(book) {
+    tuksa.forEach(book => {
         const p = document.createElement("p");
-        p.setAttribute('class',`id${x}`)
+        p.setAttribute('class',`p${x}`)
         p.textContent = `Title: ${book.book} Author: ${book.author} Pages: ${book.numberOfPages} Read status: ${book.readStatus}`;
+
+
+        // Create each div for book
+        const div = document.createElement('div')
 
 
         // Book Read Status
         const statusButton = document.createElement('button')
         statusButton.textContent = 'Change read status'
         statusButton.setAttribute('id',`changeReadButton${x}`)
-        books.appendChild(statusButton)
+        div.appendChild(statusButton)
 
 
         const button = document.createElement('button')
         button.setAttribute('class','allDeleteButtons')
-        button.setAttribute('id',`id: ${book.id}`)
+        button.setAttribute('id',`id${x}`)
         button.textContent = 'Delete Book'
-        books.appendChild(p);
-        books.appendChild(button);
+        div.appendChild(p);
+        div.appendChild(button);
+        books.appendChild(div)
+
+
+        // Read Status Button
+        statusButton.addEventListener('click', () =>{
+            myLibrary.forEach(book => {
+
+                let bookParent = statusButton.parentElement
+                const bookText = bookParent.querySelector('p')
+                if (book.id == p.className.slice(1)){
+                    if (book.readBool === true){
+                        book.readBool = false
+                        book.readStatus = 'Not Read'
+
+                        bookText.textContent = bookText.textContent.slice(0,-5)
+                        bookText.textContent += ' Not Read'      
+                    } else {
+                        book.readBool = true
+                        book.readStatus = 'Read'
+                        bookText.textContent = bookText.textContent.slice(0,-9)
+                        bookText.textContent += ' Read'
+                    }
+                    
+                }
+            })
+        });
 
         // Remove Book Button 
         button.addEventListener("click", () => {
-            const delBooks = document.querySelectorAll('.allDeleteButtons');
-            delBooks.forEach(function(bookId) {
-                if (parseInt(bookId.id.slice(3)) === myLibrary[x]['id']){
-                    myLibrary.splice(x,1);
 
-                    const delElement = document.getElementById(`${bookId.id}`)
-                    delElement.remove()
-                    const delButton = document.querySelector(`.id${x}`)
-                    delButton.remove()
-                    const delBookStatus = document.querySelector(`#changeReadButton${x}`)
-                    delBookStatus.remove()
-                
+            // Remove book from Library
+            myLibrary.forEach(bookId => {
+                if (bookId.id == button.id.slice(2)){
+                    let newArray = myLibrary.filter(obj => obj.id !== button.id.slice(2));
+                    myLibrary = newArray
                 }
-                x++
+            let parentToRemove = button.parentElement
+            parentToRemove.remove()
+
             });
-            x = 0;
         });
+        x++
     });
 }
-
 
 
 
